9b2542862379719638f72ba450e89e6a1009d114
Eduardo Cordovil <86244886+EduardoSilvaCordovil@users.noreply.github.com>
2025-03-25 12:29:06 -0300

    Primeiro comiit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>FacePass</title>
+	<link rel="manifest" href="/manifest.json" />
+	<link href="/style.css" rel="stylesheet" />
+</head>
+
+<body>
+<h1>Segurança digital com um olhar para o futuro</h1>
+
+<button id="install">Instale a PWA</button>
+<script src="/script.js"></script>
+</body>
